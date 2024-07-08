443c79d520e4106f39f5a41841aee8e3575d7371
abdul-o1 <rahman567123@gmail.com>
2024-07-08 15:09:55 +0530

    Search functionality added

---
+++ src/api.js
@@ -0,0 +1,10 @@
+export const geoApiOptions = {
+    method: 'GET',
+	headers: {
+        'x-rapidapi-key': '8671768b2bmsh46fe3aa58e224a4p194f99jsn6ad7a7c0aa80',
+		'x-rapidapi-host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
+ 
