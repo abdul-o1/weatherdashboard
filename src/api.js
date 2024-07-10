e634d903c280b505113f52e6e05f1a7a4ce60e5b
abdul-o1 <rahman567123@gmail.com>
2024-07-10 14:05:58 +0530

    added the 7 days forecast functionality

--- src/api.js
+++ src/api.js
@@ -1,10 +1,11 @@
 export const geoApiOptions = {
-    method: 'GET',
-	headers: {
-        'x-rapidapi-key': '8671768b2bmsh46fe3aa58e224a4p194f99jsn6ad7a7c0aa80',
-		'x-rapidapi-host': 'wft-geo-db.p.rapidapi.com'
-	}
+  method: "GET",
+  headers: {
+    "x-rapidapi-key": "8671768b2bmsh46fe3aa58e224a4p194f99jsn6ad7a7c0aa80",
+    "x-rapidapi-host": "wft-geo-db.p.rapidapi.com",
+  },
 };
 
-export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
- 
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "11c393cdd57d034ebb0af265f7beb498";
--- src/components/search/search.js
+++ src/components/search/search.js
@@ -7,18 +7,18 @@ const Search = ({ onSearchChange }) => {
 
   const loadOptions = async (inputValue) => {
     const response = await fetch(
-          `${GEO_API_URL}/cities?minPopulation=1000000&namePrefix=${inputValue}`,
-          geoApiOptions
-      );
-      const response_1 = await response.json();
-      return {
-          options: response_1.data.map((city) => {
-              return {
-                  value: `${city.latitude} ${city.longitude}`,
-                  label: `${city.name}, ${city.countryCode}`,
-              };
-          }),
-      };
+      `${GEO_API_URL}/cities?minPopulation=1000000&namePrefix=${inputValue}`,
+      geoApiOptions
+    );
+    const response_1 = await response.json();
+    return {
+      options: response_1.data.map((city) => {
+        return {
+          value: `${city.latitude} ${city.longitude}`,
+          label: `${city.name}, ${city.countryCode}`,
+        };
+      }),
+    };
   };
 
   const handleOnChange = (searchData) => {
